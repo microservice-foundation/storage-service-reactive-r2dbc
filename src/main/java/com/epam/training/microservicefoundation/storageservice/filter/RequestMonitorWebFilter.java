c45af820a599939843e691b9a4e392e502521901
Yusuf Murodov <yusuf_murodov@epam.com>
2023-09-04 12:34:00 +0500

    Remove unnecessary context write and trace sampling config on application.yml

--- src/main/java/com/epam/training/microservicefoundation/storageservice/filter/RequestMonitorWebFilter.java
+++ src/main/java/com/epam/training/microservicefoundation/storageservice/filter/RequestMonitorWebFilter.java
@@ -2,8 +2,6 @@ package com.epam.training.microservicefoundation.storageservice.filter;
 
 import static java.util.Optional.ofNullable;
 
-import io.micrometer.context.ContextSnapshotFactory;
-import io.micrometer.observation.contextpropagation.ObservationThreadLocalAccessor;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,10 +19,6 @@ public class RequestMonitorWebFilter implements WebFilter {
   public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
     final long startTime = System.currentTimeMillis();
     return chain.filter(exchange)
-        .contextWrite(context -> {
-          ContextSnapshotFactory.builder().build().setThreadLocalsFrom(context, ObservationThreadLocalAccessor.KEY);
-          return context;
-        })
         .doFinally(signalType -> {
           final long endTime = System.currentTimeMillis();
           final long executionTime = endTime - startTime;
